0519b27c72058e2324d2f8f879a3f546a85ae2a3
Nolewen <38526136+Nolewen@users.noreply.github.com>
2018-05-09 15:38:43 +0100

    Update profile.h

--- RedSocial1/include/profile.h
+++ RedSocial1/include/profile.h
@@ -1,3 +1,4 @@
+
 #ifndef PROFILE_H
 #define PROFILE_H
 
@@ -5,26 +6,28 @@
 
 namespace FIS{
 
-struct InfoUser{
+struct infoUser{
     std::string name;     //Nombre comun de usuario
     std::string passwd;   //Contraseña -> El cifrado y los asuntos de seguridad los dejaría para más adelante
     int edad;             //Duh
-
+    std::string phoneNumber; // Número de teléfono
+    std::string location; // Localidad o domicilio
     //...    Por si falta algo necesario
 
 };
+// Posibilidad de añadir si un usuario es menor de X edad que no se pueda crear la cuenta
 
 class Profile{
 private:
     int UserID_;        //Identificador numerico de usuario
-    InfoUser data_;
+    infoUser data_;
 
     int TLFD_;          //TimeLine file descriptor, porque somos bilingües
         //La idea es que almacene los identificadores de los mensajes propios
     std::vector<FIS::Message> MensajesUsu_;
 
 public:
-    Profile();          //Recibir -> Datos de usuario, se le solicitan al crear la cuenta
+    Profile(infoUser data_, int UserID);          //Recibir -> Datos de usuario, se le solicitan al crear la cuenta
     //Profile();        //Configurar el constructor para que pueda obtener los datos del fichero de texto también
                         //Así podemos preservar los cambios y las cuentas de usuario entre ejecuciones
     ~Profile();         //Dudo que tenga que hacer gran cosa
@@ -32,6 +35,17 @@ public:
     //Getters y setters para atributos y posibles cambios de datos que quiera hacer el usuario
 
 };
+    
+   Profile(infoUser data_, int UserID){
+       
+    ofstream profile;
+	profile.open("perfil.txt", ios::out);
+
+	//profile << "ID" << "::"<< "Nombre" << "::"<< "Constraseña" << "::"<< "Edad" << "::" << "Teléfono" << "::"<< "Localidad" << endl;
+    profile << UserID << "::"<< data_.name << "::"<< data_.passwd << "::"<< data_.edad << "::"<< data_.phoneNumber << "::"<< data_.location << endl;
+    
+       
+   }
 
 }
 
